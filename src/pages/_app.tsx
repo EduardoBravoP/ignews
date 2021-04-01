c9505692ac21fe7799de77818af3ad81ebe277d7
Eduardo Bravo <eduardobravopires@gmail.com>
2021-03-31 22:09:08 -0300

    updated styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
@@ -6,10 +7,12 @@ function MyApp({
   Component,
   pageProps
 }: AppProps) {
-  return <Component {
-    ...pageProps
-  }
-  />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps}/>
+    </>
+  )
 }
 
 export default MyApp
