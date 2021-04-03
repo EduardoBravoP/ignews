12da2fa76380718db6b84f856564c9a8d7c94ead
Eduardo Bravo <eduardobravopires@gmail.com>
2021-04-03 01:24:03 -0300

    add next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Provider as NextAuthProvider } from 'next-auth/client';
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header';
 
@@ -8,10 +9,10 @@ function MyApp({
   pageProps
 }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps}/>
-    </>
+    </NextAuthProvider>
   )
 }
 
